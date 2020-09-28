24656fecb8c5b6f99181fe9616663ed91bdd5312
Pavlo Semchyshyn <pavlosemchyshyn@gmail.com>
2020-09-28 17:01:13 +0300

    Falling ball added

--- static/game.js
+++ static/game.js
@@ -1,3 +1,24 @@
+// Token class
+class Token {
+    constructor(face1, face2) {
+        this.upperFace = face1
+        this.lowerFace = face2
+    }
+
+    rotateSides () {
+        [this.upperFace, this.lowerFace] = [this.lowerFace, this.upperFace]
+    }
+
+    flipToken() {
+        // Imitation of flipping token
+        let rotateAtOnce = Math.floor(Math.random()*2)
+        if (rotateAtOnce) {
+            this.rotateSides()
+        }
+    }
+}
+
+// Define all handler buttons and screens to be listened
 const startBtn = document.getElementById("startBtn")
 const quitBtn = document.getElementById("quitBtn")
 const againBtn = document.getElementById("againBtn")
@@ -14,36 +35,42 @@ const ball = document.getElementById("fallingBall")
 const notification = document.getElementById("notificationResult")
 
 
-console.log(startBtn)
-console.log(guessedField)
-console.log(guessedToAllField)
-console.log(totalTriedField)
-
-
-class Token {
-    constructor(face1, face2) {
-        this.upperFace = face1
-        this.lowerFace = face2
-    }
-
-    rotateSides () {
-        [this.upperFace, this.lowerFace] = [this.lowerFace, this.upperFace]
-    }
-}
-
-
+// Initiate variables to track the statistics + set tokens
 let totalTries = 0
 let guessed = 0
 let guessedToAll = 0
-let token;
-
 const tokens = [new Token("green", "green"), new Token("red", "red"), new Token("red", "green")]
 
 
-function simulateFalling() {
-    
-}
+// Initiate variables for ball movement animation
+let token, speed = 0, opposite = false, gravity = 4, distance = 0,
+ container = document.getElementById("fallingBallContainer"), limit = container.clientHeight - ball.clientHeight;
 
+// Performs the affect of falling ball
+function simulateFalling() {
+    if (opposite){
+        speed = speed > 0 ? -speed: speed
+    }
+    speed += gravity
+    distance += speed
+    ball.style.top = `${distance}px`
+    if (distance >= limit) {
+        opposite = true
+    }
+    if (speed <= 0) {
+        opposite = false
+    }
+    if (distance >= limit && speed <= 0){
+        distance = 0
+        speed = 0
+    } else {
+        requestAnimationFrame(simulateFalling)
+    }
+}
+// Finish animating falling ball
+
+
+// Logic of guess
 function game(token, choice) {
     const userCorrect = match(token, choice)
     if (userCorrect) {
@@ -57,11 +84,12 @@ function game(token, choice) {
     guessedToAllField.innerHTML = guessedToAll
 }
 
-
+// Check if token's lower face matches with user guess
 function match(token, userChoice) {
     return token.lowerFace == userChoice
 }
 
+// Randommly choose one out of three tokens
 function getRandomToken(tokens) {
     // Remove previously set colors
     ball.classList.remove("green")
@@ -70,15 +98,14 @@ function getRandomToken(tokens) {
     random = Math.floor(Math.random()*tokens.length)
     const token = tokens[random]
     // At chance 0.5 to swap sides
-    let rotateAtOnce = Math.floor(Math.random()*2)
-    if (rotateAtOnce) {
-        token.rotateSides()
-    }
+    token.flipToken()
     // Add to DOM
-    console.log(token.upperFace)
     ball.classList.add(token.upperFace)
     return token
 }
+// Finish logic
+
+// Adding event listeners
 
 startBtn.addEventListener("click", () => {
     gameScreen.classList.remove("fadeIn")
@@ -107,12 +134,17 @@ redBtn.addEventListener("click", () => {
 
 greenBtn.addEventListener("click", () => {
     game(token, "green")
+    ball.style.top = "5px"
     result.classList.remove("fadeIn")
     falling.classList.add("fadeIn")
 })
 
 againBtn.addEventListener("click", () => {
+    // prepare random token before start
     token = getRandomToken(tokens)
+    // reset ball postion
+    ball.style.top = "0px"
+    requestAnimationFrame(simulateFalling)
     result.classList.add("fadeIn")
     falling.classList.remove("fadeIn")
 })
